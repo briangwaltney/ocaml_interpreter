ac94c8ae71e220bb55780bb316bf4fe318b284d8
Brian Gwaltney <brian.gwaltney@gmail.com>
2023-06-29 16:43:50 +0300

    lexer done

--- _build/default/lib/.ocaml_int.objs/ocaml_int__Lexer.impl.d
+++ _build/default/lib/.ocaml_int.objs/ocaml_int__Lexer.impl.d
@@ -1,1 +1,1 @@
-lib/lexer.ml:
+lib/lexer.ml: Base Char In_channel List String Token
---
+++ bin/main.ml
@@ -0,0 +1,95 @@
+open Ocaml_int.Lexer
+open Ocaml_int.Token
+
+let test =
+  "let five = 5;\n\
+   let ten = 10;\n\
+  \   let add = fn(x, y) {\n\
+  \     x + y;\n\
+   };\n\
+  \   let result = add(five, ten);\n\
+  \   !-/*5;\n\
+  \   5 < 10 > 5;\n\
+  \   if (5 < 10) {\n\
+  \       return true;\n\
+  \   } else {\n\
+  \       return false;}\n\n\n\
+   10 == 10; 10 != 9;"
+;;
+
+assert (
+  tokens_of_string test
+  = [ Let
+    ; Ident "five"
+    ; Assign
+    ; Integer "5"
+    ; Semicolon
+    ; Let
+    ; Ident "ten"
+    ; Assign
+    ; Integer "10"
+    ; Semicolon
+    ; Let
+    ; Ident "add"
+    ; Assign
+    ; Function
+    ; LeftParen
+    ; Ident "x"
+    ; Comma
+    ; Ident "y"
+    ; RightParen
+    ; LeftBrace
+    ; Ident "x"
+    ; Plus
+    ; Ident "y"
+    ; Semicolon
+    ; RightBrace
+    ; Semicolon
+    ; Let
+    ; Ident "result"
+    ; Assign
+    ; Ident "add"
+    ; LeftParen
+    ; Ident "five"
+    ; Comma
+    ; Ident "ten"
+    ; RightParen
+    ; Semicolon
+    ; Bang
+    ; Minus
+    ; Slash
+    ; Asterisk
+    ; Integer "5"
+    ; Semicolon
+    ; Integer "5"
+    ; LessThan
+    ; Integer "10"
+    ; GreaterThan
+    ; Integer "5"
+    ; Semicolon
+    ; If
+    ; LeftParen
+    ; Integer "5"
+    ; LessThan
+    ; Integer "10"
+    ; RightParen
+    ; LeftBrace
+    ; Return
+    ; True
+    ; Semicolon
+    ; RightBrace
+    ; Else
+    ; LeftBrace
+    ; Return
+    ; False
+    ; Semicolon
+    ; RightBrace
+    ; Integer "10"
+    ; Equal
+    ; Integer "10"
+    ; Semicolon
+    ; Integer "10"
+    ; NotEqual
+    ; Integer "9"
+    ; Semicolon
+    ])
